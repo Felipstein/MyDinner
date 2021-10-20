04ea9876c98c8066f51f762c4b70b031881a6682
Felipstein <luisfelipe-oliveira@outlook.com.br>
2021-10-19 22:14:49 -0300

    Home Page criada e Estilos Resetados (styled-reset instalado)

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+        primary: '#b30000',
+        background: '#00000014',
+        text: '#000000b3',
+        sucess: '#135e01'
+    },
+    fonts: {
+        regular: 'Roboto, sans-serif'
+    }
+};
+
+export default theme;
